8fa229e12918a8e2a5f2ede4b2bb5983eb970740
MarsGoatzz <sid.310@icloud.com>
2020-05-10 16:25:33 -0700

    Move new udpate notification
    Currently the new update notification if an announcement is less than seven days,
    is on the trailing end of the list widget. While this works perfectly on lager devices,
    the view is skewed on smalled mobile devices.
    
    To fix the issue, the trailing update is removed and instead the listview is wrapped in a
    Column which has a Text on top of the listview card with 'NEW UPDATE'.

--- lib/announcements.dart
+++ lib/announcements.dart
@@ -30,6 +30,8 @@ class _AnnouncementsState extends State<Announcements> {
 
   @override
   Widget build(BuildContext context) {
+    double cardPadding = MediaQuery.of(context).size.width < 600 ? 25 : 50;
+
     return Scaffold(
       appBar: AppBar(
         title: Text(
@@ -41,30 +43,54 @@ class _AnnouncementsState extends State<Announcements> {
         child: jsonData == null
             ? Center(child: CircularProgressIndicator())
             : ListView.builder(
-                padding: EdgeInsets.all(50),
+                padding: EdgeInsets.all(cardPadding),
                 itemCount: items.length,
                 itemBuilder: (BuildContext context, int index) {
                   return Card(
-                    color: _isWithinOneWeek(items[index]['pubDate'])? Colors.redAccent : Colors.transparent,
-                    elevation: _isWithinOneWeek(items[index]['pubDate'])? 20 : 10,
-                    shadowColor: _isWithinOneWeek(items[index]['pubDate'])? Colors.red : Colors.black,
+                    color: _isWithinOneWeek(items[index]['pubDate'])
+                        ? Colors.redAccent
+                        : Colors.transparent,
+                    elevation:
+                        _isWithinOneWeek(items[index]['pubDate']) ? 20 : 10,
+                    shadowColor: _isWithinOneWeek(items[index]['pubDate'])
+                        ? Colors.red
+                        : Colors.black,
                     margin: EdgeInsets.fromLTRB(20, 10, 20, 10),
-                    child: ListTile(
-                      hoverColor: Colors.amberAccent,
-                      contentPadding: EdgeInsets.all(20.0),
-                      title: new Text(
-                        items[index]['title'],
-                        style: GoogleFonts.josefinSans(),
-                      ),
-                      subtitle: Text(items[index]['categories'].join(', ')),
-                      leading: new Image.network(
-                        items[index]['thumbnail'],
-                        fit: BoxFit.cover,
-                        height: 400.0,
-                        width: 50.0,
-                      ),
-                      trailing: _isWithinOneWeek(items[index]['pubDate'])? Text('NEW UPDATE', style: GoogleFonts.josefinSans(color: Colors.black)) : Text(''),
-                      onTap: () => launch(items[index]['link']),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: <Widget>[
+                        Visibility(
+                          visible: _isWithinOneWeek(items[index]['pubDate']),
+                          child: Container(
+                            width: double.infinity,
+                            color: Colors.redAccent,
+                            child: Padding(
+                              padding: EdgeInsets.fromLTRB(20, 20, 0, 10),
+                              child: Text(
+                                'NEW UPDATE',
+                                style:
+                                    GoogleFonts.josefinSans(color: Colors.black),
+                              ),
+                            ),
+                          ),
+                        ),
+                        ListTile(
+                          hoverColor: Colors.amberAccent,
+                          contentPadding: EdgeInsets.all(20.0),
+                          title: new Text(
+                            items[index]['title'],
+                            style: GoogleFonts.josefinSans(),
+                          ),
+                          subtitle: Text(items[index]['categories'].join(', ')),
+                          leading: new Image.network(
+                            items[index]['thumbnail'],
+                            fit: BoxFit.cover,
+                            height: 400.0,
+                            width: 50.0,
+                          ),
+                          onTap: () => launch(items[index]['link']),
+                        ),
+                      ],
                     ),
                   );
                 },
@@ -75,6 +101,6 @@ class _AnnouncementsState extends State<Announcements> {
 
   bool _isWithinOneWeek(String date) {
     var publishDate = DateTime.parse(date);
-    return DateTime.now().difference(publishDate).inDays < 7; 
+    return DateTime.now().difference(publishDate).inDays < 7;
   }
 }
