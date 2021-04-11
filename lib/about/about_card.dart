8f2f19fbbaedd938b363802baee525e4ca3d53f2
MarsGoatzz <sid.310@icloud.com>
2021-04-11 15:48:57 -0700

    Make changes to info about Lu

--- lib/about/about_card.dart
+++ lib/about/about_card.dart
@@ -34,7 +34,7 @@ class AboutCard extends StatelessWidget {
           height: 20,
         ),
         Text(
-          'Flutter Vancouver was created by Siddharth Molleti after falling in love with Flutter and his interest for creating a community and teaching others came together. In addition to being a speaker, Sid actively maintains and organizes events in the community. The next person to make an impact in the group is Boris Schneiderman. Boris has been involved since the beginning and is the Gandalf of our group. On top of having high standards for teaching others, when someone has a query, he goes above and beyond to help them. The group is very lucky to have him. Next up is Lu Tang, Lu is a self learner and previously has had no background on programming. She picked up Flutter to kick start her career in tech and now is actively a Flutter developer. She is truly an inspiration for women out there. All of us live and work in Vancouver.',
+          'Flutter Vancouver was created by Siddharth Molleti after falling in love with Flutter and his interest for creating a community and teaching others came together. In addition to being a speaker, Sid actively maintains and organizes events in the community. The next person to make an impact in the group is Boris Schneiderman. Boris has been involved since the beginning and is the Gandalf of our group. On top of having high standards for teaching others, when someone has a query, he goes above and beyond to help them. The group is very lucky to have him. Next up is Lu Tang, Lu is a self learner and and after working on different fields in tech industry, she chose Flutter as her career as she believes the value in Flutter. She is passionate about creating great software applications that bring values to our life and helping others to learn Flutter. She is truly an inspiration for women out there. All of us live and work in Vancouver.',
           style: StyleConstants.kDefaultTextStyle,
         ),
         SizedBox(
@@ -94,7 +94,7 @@ class AboutCard extends StatelessWidget {
                           "https://firebasestorage.googleapis.com/v0/b/fluttervancity.appspot.com/o/lu.jpg?alt=media&token=fd2fd02c-317c-48eb-bdbf-dbff97279a5d",
                       name: 'Lu Tang',
                       message:
-                          'Co-Organizer & Flutter Developer at Payload Technologies Inc.',
+                          'Co-Organizer & Flutter Developer at various tech startups',
                       twitterHandle: 'https://twitter.com/FlutterLulu',
                       linkedInHandle: 'https://www.linkedin.com/in/lutang123/',
                       email: 'lutang908@gmail.com',
