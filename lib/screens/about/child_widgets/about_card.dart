02735c17749830774934fa2cec19d3b0d0162f54
MarsGoatz <sid.510@icloud.com>
2022-02-09 07:51:16 -0800

    Add sebasitan as co-organizer

--- lib/screens/about/child_widgets/about_card.dart
+++ lib/screens/about/child_widgets/about_card.dart
@@ -9,36 +9,53 @@ class AboutCard extends StatelessWidget {
     return FlexCard(
       title: 'About',
       message:
-          'Flutter Canada was created by Siddharth Molleti after falling in love with Flutter and his interest for creating a community and teaching others came together. In addition to being a speaker, Sid actively maintains and organizes events in the community. If you are interested in being co-organizer, please reach out!',
+          'Flutter Canada was created by Siddharth Molleti after falling in love with Flutter and the concept of "everything is a widget". Sid started the group in February 2020 and grew the community from ground up for two years and grew the community from 0 to almost 500 people. Initially Sid started with hosting and giving talks in Vancouver and then expanded to Toronto at which point the community was rebranded to Flutter Canada. Alan joined as organizer on January 2022 and is now the current lead of the group and is taking the community to new heights. If you are interested in being an organizer, please reach out!',
       items: [
         FlexResponsiveItem(
-            flex: 2,
-            child: PersonDetails(
-              networkImageUrl:
-                  "https://firebasestorage.googleapis.com/v0/b/fluttervancity.appspot.com/o/sid.png?alt=media&token=f13cccbc-f093-4f60-829c-3b4cee60e4f2",
-              name: 'Siddharth Molleti',
-              message: 'Founder & Organizer',
-              twitterHandle: 'https://twitter.com/marsgoatzz',
-              linkedInHandle: 'https://www.linkedin.com/in/smllti',
-              email: 'sid.310@icloud.com',
-              mediumLink: 'https://marsgoat.medium.com/',
-              webLink: 'https://marsgoat.com',
-              borderColor: Colors.cyan,
-            )),
+          flex: 2,
+          child: PersonDetails(
+            networkImageUrl:
+                "https://firebasestorage.googleapis.com/v0/b/fluttervancity.appspot.com/o/sid.png?alt=media&token=64e1f980-b690-49d5-b7ca-acfc9a5d67c9",
+            name: 'Siddharth Molleti',
+            message: 'Founder & Mentor',
+            twitterHandle: 'https://twitter.com/marsgoatzz',
+            linkedInHandle: 'https://www.linkedin.com/in/smllti',
+            email: 'sid.310@icloud.com',
+            mediumLink: 'https://marsgoat.medium.com/',
+            webLink: 'https://marsgoat.com',
+            borderColor: Colors.cyan,
+          ),
+        ),
         FlexResponsiveItem(
-            flex: 2,
-            child: PersonDetails(
-              networkImageUrl:
-                  "https://firebasestorage.googleapis.com/v0/b/fluttervancity.appspot.com/o/alan.png?alt=media&token=8aabe801-f901-4241-86f3-a8f9f4a390d2",
-              name: 'Alan Hill',
-              message: 'Co-Organizer',
-              twitterHandle: 'https://twitter.com/palge',
-              linkedInHandle: 'https://www.linkedin.com/in/alanjhill/',
-              email: 'alan@octopology.com',
-              mediumLink: 'https://medium.com/@alanjhill',
-              webLink: null,
-              borderColor: Colors.cyan,
-            )),
+          flex: 2,
+          child: PersonDetails(
+            networkImageUrl:
+                "https://firebasestorage.googleapis.com/v0/b/fluttervancity.appspot.com/o/alan.jpg?alt=media&token=81805921-5bd7-4ee6-93b6-5764ad281de5",
+            name: 'Alan Hill',
+            message: 'Lead Organizer',
+            twitterHandle: 'https://twitter.com/palge',
+            linkedInHandle: 'https://www.linkedin.com/in/alanjhill/',
+            email: 'alan@octopology.com',
+            mediumLink: 'https://medium.com/@alanjhill',
+            webLink: null,
+            borderColor: Colors.cyan,
+          ),
+        ),
+        FlexResponsiveItem(
+          flex: 2,
+          child: PersonDetails(
+            networkImageUrl:
+                "https://firebasestorage.googleapis.com/v0/b/fluttervancity.appspot.com/o/sedwoo.png?alt=media&token=17da1cf7-04c5-4acc-9ba9-ba201ed530cb",
+            name: 'Sebastian Dwornik',
+            message: 'Co-Organizer',
+            email: 'sebastian@appliedpda.com',
+            mediumLink: null,
+            twitterHandle: null,
+            linkedInHandle: null,
+            webLink: null,
+            borderColor: Colors.cyan,
+          ),
+        ),
       ],
     );
   }
