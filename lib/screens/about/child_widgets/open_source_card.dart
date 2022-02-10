f7d7a0f6812ba761beaed0471944dadae8b4a73c
MarsGoatz <sid.510@icloud.com>
2022-02-10 07:22:03 -0800

    Remove ref to FV website in open source card

--- lib/screens/about/child_widgets/open_source_card.dart
+++ lib/screens/about/child_widgets/open_source_card.dart
@@ -13,76 +13,41 @@ class OpenSourceCard extends StatelessWidget {
           'Flutter Canada was forked from Flutter Vancouver\'s open source code. Feel free to check it out and contribute!',
       items: [
         FlexResponsiveItem(
-            flex: 2,
-            child: Column(
-              mainAxisSize: MainAxisSize.min,
-              children: [
-                MouseRegion(
-                  cursor: SystemMouseCursors.click,
-                  child: GestureDetector(
-                    onTap: () =>
-                        launch('https://github.com/MarsGoatz/birdforce'),
-                    child: Image.asset(
-                      "assets/github.png",
-                      height: 200,
-                      width: 200,
-                    ),
+          flex: 2,
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              MouseRegion(
+                cursor: SystemMouseCursors.click,
+                child: GestureDetector(
+                  onTap: () => launch('https://github.com/MarsGoatz/birdforce'),
+                  child: Image.asset(
+                    "assets/github.png",
+                    height: 200,
+                    width: 200,
                   ),
                 ),
-                SizedBox(height: 20),
-                Padding(
-                  padding: const EdgeInsets.only(left: 15.0),
-                  child: OutlineButton(
-                      padding: EdgeInsets.all(16),
-                      borderSide: BorderSide(width: 2.0, color: Colors.red),
-                      color: Colors.redAccent,
-                      onPressed: () {
-                        launch('https://github.com/MarsGoatz/birdforce');
-                      },
-                      child: Text(
-                        'Github',
-                        style: StyleConstants.kButtonTextStyle,
-                      ),
-                      shape: new RoundedRectangleBorder(
-                          borderRadius: new BorderRadius.circular(20.0))),
-                ),
-              ],
-            )),
-        FlexResponsiveItem(
-            flex: 2,
-            child: Column(
-              mainAxisSize: MainAxisSize.min,
-              children: [
-                MouseRegion(
-                  cursor: SystemMouseCursors.click,
-                  child: GestureDetector(
-                    onTap: () => launch('https://fluttervancouver.ca'),
-                    child: Image.asset(
-                      "assets/vancouver.png",
-                      height: 200,
-                      width: 200,
+              ),
+              SizedBox(height: 20),
+              Padding(
+                padding: const EdgeInsets.only(left: 15.0),
+                child: OutlineButton(
+                    padding: EdgeInsets.all(16),
+                    borderSide: BorderSide(width: 2.0, color: Colors.red),
+                    color: Colors.redAccent,
+                    onPressed: () {
+                      launch('https://github.com/MarsGoatz/birdforce');
+                    },
+                    child: Text(
+                      'Github',
+                      style: StyleConstants.kButtonTextStyle,
                     ),
-                  ),
-                ),
-                SizedBox(height: 20),
-                Padding(
-                  padding: const EdgeInsets.only(left: 15.0),
-                  child: OutlineButton(
-                      padding: EdgeInsets.all(16),
-                      borderSide: BorderSide(width: 2.0, color: Colors.red),
-                      color: Colors.redAccent,
-                      onPressed: () {
-                        launch('https://fluttervancouver.ca');
-                      },
-                      child: Text(
-                        'Website',
-                        style: StyleConstants.kButtonTextStyle,
-                      ),
-                      shape: new RoundedRectangleBorder(
-                          borderRadius: new BorderRadius.circular(20.0))),
-                ),
-              ],
-            )),
+                    shape: new RoundedRectangleBorder(
+                        borderRadius: new BorderRadius.circular(20.0))),
+              ),
+            ],
+          ),
+        ),
       ],
     );
   }
